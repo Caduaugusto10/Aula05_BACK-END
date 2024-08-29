6cab89bf871bd5648567a1575fe17bbffd20bc98
Carlos Eduardo <cadu.augusto10@gmail.com>
2024-08-29 15:51:40 -0300

    feat: add some requests

--- src/server.js
+++ src/server.js
@@ -9,8 +9,53 @@ const app = express()
 
 app.use(express.json())
 
+const guloseimas = [
+    {
+        id: 1,
+        nome: "Trufa",
+        preco: 8.5,
+    },
+    {
+        id: 2,
+        nome: "Palha italiana",
+        preco: 5.90
+    },
+    {
+        id: 3,
+        nome: "Bala",
+        preco: 1.5
+    }
+]
+const filmesMarcantes = [
+    {
+        id: 1001,
+        titulo: "Fast and Furious",
+        genero: "Action",
+        emCartaz: false
+    },
+    {
+        id: 1002,
+        titulo: "Procurando Nemo",
+        genero: "Animação",
+        emCartaz: false
+    },
+    {
+        id: 1003,
+        titulo: "It a Coisa",
+        genero: "Terror",
+        emCartaz: false
+    }
+]
 app.get("/", (req, res) => {
     return res.status(200).send({ message: "Hello, World!"})
+})
+
+app.get("/doces", (req, res) => {
+    return res.status(200).send({guloseimas})
+})
+
+app.get("/filmes", (req, res) => {
+    return res.status(200).send({filmesMarcantes})
 })
 
 app.listen(port, () => {
