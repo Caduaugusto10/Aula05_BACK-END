1202e45567ebf8fc45ccb66cbefb06e11f60310f
Carlos Eduardo <cadu.augusto10@gmail.com>
2024-09-05 09:52:38 -0300

    feat: create get by id method

--- src/routes/doces.routes.js
+++ src/routes/doces.routes.js
@@ -19,11 +19,12 @@ const guloseimas = [
         preco: 1.5
     }
 ]
-
+//Rota para buscar todos os elementos do array guloseimas
 docesRoutes.get("/", (req, res) => {
     return res.status(200).send({guloseimas})
 })
 
+//Rota para criar novas guloseimas
 docesRoutes.post("/", (req, res) => {
     const {nome, preco} = req.body
 
@@ -35,6 +36,21 @@ docesRoutes.post("/", (req, res) => {
     guloseimas.push(novoDoce)
 
     return res.status(201).send({guloseimas})
+})
+
+//Rota para buscar um elemento específico do array guloseimas
+docesRoutes.get("/:id", (req, res) => {
+    const { id } = req.params
+
+    const guloseima = guloseimas.find ((doce) => doce.id === Number(id))
+
+    console.log(guloseima)
+
+    if (!guloseima) {
+        return res.status(404).send({ message: "Guloseima não encontrada!" })
+    }
+
+    return res.status(200).send(guloseima)
 })
 
 export default docesRoutes
